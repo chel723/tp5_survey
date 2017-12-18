52ebc86036fbda96796dea002249b2e3d46256a8
chel723 <chel723@gmail.com>
2017-12-18 21:14:42 +0100

    add test

--- src/__tests__/Test.js
+++ src/__tests__/Test.js
@@ -8,3 +8,23 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+it('add a choice to my list',()=>{
+  const initalChoiceList = [
+    {id:1, name: 'First Choice', vote: 1, percentage: null},
+    {id:2, name: 'Second Choice', vote: 1, percentage: null}
+  ]
+  expect(newChoice).toEqual(expectedChoice);
+});
+
+it('add a choice using helper',()=>{
+  const initalChoiceList =[
+    {id:1, name: 'First Choice', vote: 1, percentage: null},
+    {id:2, name: 'Second Choice', vote: 1, percentage: null}
+  ]
+  const expectedChoice = [...initalChoiceList];
+
+  choiceHelper.addChoice(initalChoiceList,2);
+  expect(initalChoiceList).toEqual(expectedChoice);
+  
+});
